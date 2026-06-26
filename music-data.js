7ab90cd8a4239cbfdf4f5cc9c49933108c932c55
InfiniteDOJ <austinparo530@gmail.com>
2026-06-26 19:11:14 -0400

    Create music-data.js

---
+++ music-data.js
@@ -0,0 +1,12 @@
+window.musicLibrary = [
+    {
+        title: "Sample song",
+        genre: "Sample genre",
+        audioUrl: "audio/jersey_club_beat.mp3" 
+    },
+    {
+        title: "Sample song",
+        genre: "Sample genre",
+        audioUrl: "audio/lofi_vibe.mp3"
+    }
+];
